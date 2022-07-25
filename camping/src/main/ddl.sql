d9d20ac20ec2849864df245dcc0f82168e5a9886
MJKIM <mjkim8764@gmail.com>
2022-07-25 10:53:11 +0900

    Modify Mapper To Uppercase

--- camping/src/main/ddl.sql
+++ camping/src/main/ddl.sql
@@ -12,46 +12,46 @@ drop table GOODS_SAMPLE;
 
 # Goods 테이블
 create table GOODS(
-    id int(10) not null auto_increment primary key,
-    prd_id_tmp varchar(50) default '-1',
-    category varchar(50),
-    name varchar(50),
-    image_url varchar(4000),
-    price int(10),
-    lender_email varchar(50),
-    reviews int(10),
-    grade double
+    ID int(10) not null auto_increment primary key,
+    PRD_ID_TMP varchar(50) default '-1',
+    CATEGORY varchar(50),
+    NAME varchar(50),
+    IMAGE_URL varchar(4000),
+    PRICE int(10),
+    LENDER_EMAIL varchar(50),
+    REVIEWS int(10),
+    GRADE double
 );
 
 # Review 테이블
 create table REVIEW(
-    id int(10) not null auto_increment primary key,
-    prd_id int(10),
-    prd_id_tmp varchar(50) default '0',
-    email varchar(100),
-    grade int(10),
-    review varchar(4000)
+    ID int(10) not null auto_increment primary key,
+    PRD_ID int(10),
+    PRD_ID_TMP varchar(50) default '0',
+    EMAIL varchar(100),
+    GRADE int(10),
+    REVIEW varchar(4000)
 );
 
 # Member 테이블
 create table MEMBER(
-    email varchar(50) not null primary key,
-    password varchar(255) not null,
-    name varchar(50) not null,
-    traded int(10) default 0,
-    grade double default 3
+    EMAIL varchar(50) not null primary key,
+    PASSWORD varchar(255) not null,
+    NAME varchar(50) not null,
+    TRADED int(10) default 0,
+    GRADE double default 3
 );
 
 # Rental 테이블
 CREATE TABLE RENTAL(
-    id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    prd_id INT(10),
-    lender_email VARCHAR(20),
-    borrower_email VARCHAR(20),
-    rental_start_date VARCHAR(20),
-    rental_end_date VARCHAR(20),
-    message VARCHAR(4000),
-    rent_status VARCHAR(20)
+    ID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    PRD_ID INT(10),
+    LENDER_EMAIL VARCHAR(20),
+    BORROWER_EMAIL VARCHAR(20),
+    RENTAL_START_DATE VARCHAR(20),
+    RENTAL_END_DATE VARCHAR(20),
+    MESSAGE VARCHAR(4000),
+    RENT_STATUS VARCHAR(20)
 );
 
 # Code 테이블
